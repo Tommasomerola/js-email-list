6d8c7019bcc2cd6eb43c79bf5462c6ea89c98e14
TOMMASOMEROLA <MEROLATOMMASO77@GMAIL.COM>
2025-01-22 16:02:30 +0100

    fai axios.get

--- js/script.js
+++ js/script.js
@@ -3,4 +3,11 @@
 // generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
 
 // dichiaro la variabile in cui mettere le mail 
-const listaEmail = document.getElementById("container")
+const listaEmail = document.getElementById("container");
+
+// ciclo for per generare le mail
+for (let i = 0; i < 10; i++) {
+    // get per formire l'apposita API
+    axios.get( "https://flynn.boolean.careers/exercises/api/random/mail" )
+    
+}
