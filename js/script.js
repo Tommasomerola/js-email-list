0d124053be2ff70ba3a0e172f6f282e11448a92c
TOMMASOMEROLA <MEROLATOMMASO77@GMAIL.COM>
2025-01-22 15:57:56 +0100

    fai const

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 // Attraverso l’apposita API di Boolean
 
 // generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+
+// dichiaro la variabile in cui mettere le mail 
+const listaEmail = document.getElementById("container")
